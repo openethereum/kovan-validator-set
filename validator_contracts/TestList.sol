94646e97a7591b3a79b596522e2cc73c65ddfe4d
keorn <pczaban@gmail.com>
2016-12-22 17:40:45 +0100

    change to cameltoe

--- validator_contracts/TestList.sol
+++ validator_contracts/TestList.sol
@@ -5,17 +5,17 @@ contract TestList {
     address[] public validators = [0x7d577a597b2742b498cb5cf0c26cdcd726d39e6e, 0x82a978b3f5962a5b0957d9ee9eef472ee55b42f1];
     
     // Called on every block to update node validator list.
-    function get_validators() constant returns (address[]) {
+    function getValidators() constant returns (address[]) {
         return validators;
     }
     
     // Expand the list of validators.
-    function add_validator(address validator) {
+    function addValidator(address validator) {
         validators.push(validator);
     }
 
     // Remove a validator from the list.
-    function remove_validator(uint index) {
+    function removeValidator(uint index) {
         validators[index] = validators[validators.length-1];
         delete validators[validators.length-1];
         validators.length--;
