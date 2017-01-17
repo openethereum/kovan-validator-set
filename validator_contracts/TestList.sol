2b51285a518ec69f33102e34b3a9f4e9ac6b3fcd
keorn <pczaban@gmail.com>
2017-01-17 18:36:20 +0100

    update reporting interface

--- validator_contracts/TestList.sol
+++ validator_contracts/TestList.sol
@@ -1,23 +1,36 @@
 pragma solidity ^0.4.6;
 
 contract TestList {
-		// Accounts used for testing: "0".sha3() and "1".sha3()
-    address[] public validators = [0x7d577a597b2742b498cb5cf0c26cdcd726d39e6e, 0x82a978b3f5962a5b0957d9ee9eef472ee55b42f1];
+	// Accounts used for testing: "0".sha3() and "1".sha3()
+	address[] public validators = [0x7d577a597b2742b498cb5cf0c26cdcd726d39e6e, 0x82a978b3f5962a5b0957d9ee9eef472ee55b42f1];
+	mapping(address => uint) indices;
+	address public disliked;
+	
+	function TestList() {
+	    for (uint i = 0; i < validators.length; i++) {
+	        indices[validators[i]] = i;
+	    }
+	}
     
-    // Called on every block to update node validator list.
+	// Called on every block to update node validator list.
     function getValidators() constant returns (address[]) {
-        return validators;
-    }
-    
-    // Expand the list of validators.
-    function addValidator(address validator) {
-        validators.push(validator);
-    }
+		return validators;
+	}
+ 
+	// Expand the list of validators.
+	function addValidator(address validator) {
+		validators.push(validator);
+	}
 
-    // Remove a validator from the list.
-    function removeValidator(uint index) {
-        validators[index] = validators[validators.length-1];
-        delete validators[validators.length-1];
-        validators.length--;
-    }
+	// Remove a validator from the list.
+	function reportMalicious(address validator) {
+		validators[indices[validator]] = validators[validators.length-1];
+		delete indices[validator];
+		delete validators[validators.length-1];
+		validators.length--;
+	}
+	
+	function reportBenign(address validator) {
+	    disliked = validator;
+	}
 }
