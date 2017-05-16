354ea988dd4944e3611586bd36c582600198ef2a
keorn <pczaban@gmail.com>
2017-05-16 12:47:42 +0100

    comments and fallback

--- validator_contracts/Kovan.sol
+++ validator_contracts/Kovan.sol
@@ -57,14 +57,23 @@ contract MajorityList is ValidatorSet {
     // Each validator is initially supported by all others.
     function MajorityList() {
         validatorsList = [
+            // Etherscan
             0x00D6Cc1BA9cf89BD2e58009741f4F7325BAdc0ED,
+            // Attores
             0x00427feae2419c15b89d1c21af10d1b6650a4d3d,
+            // TenX (OneBit)
             0x4Ed9B08e6354C70fE6F8CB0411b0d3246b424d6c,
+            // Melonport
             0x0020ee4Be0e2027d76603cB751eE069519bA81A1,
+            // Parity
             0x0010f94b296a852aaac52ea6c5ac72e03afd032d,
+            // DigixGlobal
             0x007733a1FE69CF3f2CF989F81C7b4cAc1693387A,
+            // Maker
             0x00E6d2b931F55a3f1701c7389d592a7778897879,
+            // Aurel
             0x00e4a10650e5a6D6001C38ff8E64F97016a1645c,
+            // GridSingularity
             0x00a0a24b9f0e5ec7aa4c7389b8302fd0123194de
         ];
 
@@ -315,16 +324,16 @@ contract MajorityList is ValidatorSet {
     }
 
     // Fallback function throws when called.
-    function() payable {
+    function() {
         throw;
     }
 }
 
 library AddressSet {
-    // Tracks the amount of support for a given address.
+    // Tracks the number of votes from different addresses.
     struct Data {
         uint count;
-        // Keeps track of who voted for a given address at a given block, prevent double voting.
+        // Keeps track of who voted, prevents double vote.
         mapping(address => bool) inserted;
     }
 
