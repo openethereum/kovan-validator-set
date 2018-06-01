68cc36fce4934335592528ac054d1306930e065e
André Silva <andre.beat@gmail.com>
2018-06-01 17:05:58 +0100

    fix code style issues

--- contracts/OwnedSet.sol
+++ contracts/OwnedSet.sol
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.22;
 
 import "./interfaces/Owned.sol";
 import "./interfaces/ValidatorSet.sol";
@@ -20,9 +20,29 @@ import "./interfaces/ValidatorSet.sol";
 
 // Owner can add or remove validators.
 contract OwnedSet is Owned, ValidatorSet {
+	// TYPES
+	struct AddressStatus {
+		bool isIn;
+		uint index;
+	}
+
 	// EVENTS
 	event Report(address indexed reporter, address indexed reported, bool indexed malicious);
 	event ChangeFinalized(address[] currentSet);
+
+	// STATE
+
+	// System address, used by the block sealer.
+	address constant SYSTEM_ADDRESS = 0xffffFFFfFFffffffffffffffFfFFFfffFFFfFFfE;
+	uint public recentBlocks = 20;
+
+	// Current list of addresses entitled to participate in the consensus.
+	address[] validators;
+	address[] pending;
+	mapping(address => AddressStatus) pendingStatus;
+
+	// Was the last validator change finalized. Implies validators == pending
+	bool public finalized;
 
 	// MODIFIERS
 	modifier onlySystemAndNotFinalized() {
@@ -56,23 +76,9 @@ contract OwnedSet is Owned, ValidatorSet {
 		_;
 	}
 
-	struct AddressStatus {
-		bool isIn;
-		uint index;
-	}
-
-	// System address, used by the block sealer.
-	address constant SYSTEM_ADDRESS = 0xffffFFFfFFffffffffffffffFfFFFfffFFFfFFfE;
-	uint public recentBlocks = 20;
-
-	// Current list of addresses entitled to participate in the consensus.
-	address[] validators;
-	address[] pending;
-	mapping(address => AddressStatus) pendingStatus;
-	// Was the last validator change finalized. Implies validators == pending
-	bool public finalized;
-
-	function OwnedSet(address[] _initial) public {
+	constructor(address[] _initial)
+		public
+	{
 		pending = _initial;
 		for (uint i = 0; i < _initial.length - 1; i++) {
 			pendingStatus[_initial[i]].isIn = true;
@@ -81,16 +87,11 @@ contract OwnedSet is Owned, ValidatorSet {
 		validators = pending;
 	}
 
-	// Called to determine the current set of validators.
-	function getValidators() constant public returns (address[]) {
-		return validators;
-	}
-
-	function getPending() constant public returns (address[]) {
-		return pending;
-	}
-
-	function finalizeChange() public onlySystemAndNotFinalized {
+	// Called when an initiated change reaches finality and is activated.
+	function finalizeChange()
+		external
+		onlySystemAndNotFinalized
+	{
 		validators = pending;
 		finalized = true;
 		emit ChangeFinalized(getValidators());
@@ -99,7 +100,11 @@ contract OwnedSet is Owned, ValidatorSet {
 	// OWNER FUNCTIONS
 
 	// Add a validator.
-	function addValidator(address _validator) public onlyOwner isNotPending(_validator) {
+	function addValidator(address _validator)
+		external
+		onlyOwner
+		isNotPending(_validator)
+	{
 		pendingStatus[_validator].isIn = true;
 		pendingStatus[_validator].index = pending.length;
 		pending.push(_validator);
@@ -107,7 +112,11 @@ contract OwnedSet is Owned, ValidatorSet {
 	}
 
 	// Remove a validator.
-	function removeValidator(address _validator) public onlyOwner isPending(_validator) {
+	function removeValidator(address _validator)
+		external
+		onlyOwner
+		isPending(_validator)
+	{
 		pending[pendingStatus[_validator].index] = pending[pending.length - 1];
 		delete pending[pending.length - 1];
 		pending.length--;
@@ -117,24 +126,61 @@ contract OwnedSet is Owned, ValidatorSet {
 		initiateChange();
 	}
 
-	function setRecentBlocks(uint _recentBlocks) public onlyOwner {
+	function setRecentBlocks(uint _recentBlocks)
+		external
+		onlyOwner
+	{
 		recentBlocks = _recentBlocks;
 	}
 
 	// MISBEHAVIOUR HANDLING
 
-	// Called when a validator should be removed.
-	function reportMalicious(address _validator, uint _blockNumber, bytes /* _proof */) public onlyOwner isRecent(_blockNumber) {
+	// Report that a validator has misbehaved maliciously.
+	function reportMalicious(address _validator, uint _blockNumber, bytes _proof)
+		external
+		onlyOwner
+		isRecent(_blockNumber)
+	{
 		emit Report(msg.sender, _validator, true);
 	}
 
 	// Report that a validator has misbehaved in a benign way.
-	function reportBenign(address _validator, uint _blockNumber) public onlyOwner isValidator(_validator) isRecent(_blockNumber) {
+	function reportBenign(address _validator, uint _blockNumber)
+		external
+		onlyOwner
+		isValidator(_validator)
+		isRecent(_blockNumber)
+	{
 		emit Report(msg.sender, _validator, false);
 	}
 
+	// GETTERS
+
+	// Called to determine the current set of validators.
+	function getValidators()
+		public
+		view
+		returns (address[])
+	{
+		return validators;
+	}
+
+	// Called to determine the pending set of validators.
+	function getPending()
+		public
+		view
+		returns (address[])
+	{
+		return pending;
+	}
+
+	// PRIVATE
+
 	// Log desire to change the current list.
-	function initiateChange() private whenFinalized {
+	function initiateChange()
+		private
+		whenFinalized
+	{
 		finalized = false;
 		// solium-disable-next-line security/no-block-members
 		emit InitiateChange(blockhash(block.number - 1), getPending());
--- contracts/interfaces/ValidatorSet.sol
+++ contracts/interfaces/ValidatorSet.sol
@@ -12,10 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-pragma solidity ^0.4.21;
+pragma solidity ^0.4.22;
 
 
-contract ValidatorSet {
+interface ValidatorSet {
 	/// Issue this log event to signal a desired change in validator set.
 	/// This will not lead to a change in active validator set until
 	/// finalizeChange is called.
@@ -28,19 +28,28 @@ contract ValidatorSet {
 	/// signal will not be recognized.
 	event InitiateChange(bytes32 indexed _parentHash, address[] _newSet);
 
-	/// Get current validator set (last enacted or initial if no changes ever made)
-	function getValidators() public constant returns (address[]);
-
 	/// Called when an initiated change reaches finality and is activated.
-	/// Only valid when msg.sender == SYSTEM (EIP96, 2**160 - 2)
+	/// Only valid when msg.sender == SYSTEM (EIP96, 2**160 - 2).
 	///
 	/// Also called when the contract is first enabled for consensus. In this case,
 	/// the "change" finalized is the activation of the initial set.
-	function finalizeChange() public;
+	function finalizeChange()
+		external;
 
-	// Reporting functions: operate on current validator set.
-	// malicious behavior requires proof, which will vary by engine.
+	/// Reports benign misbehavior of validator of the current validator set
+	/// (e.g. validator offline).
+	function reportBenign(address validator, uint256 blockNumber)
+		external;
 
-	function reportBenign(address validator, uint256 blockNumber) public;
-	function reportMalicious(address validator, uint256 blockNumber, bytes proof) public;
+	/// Reports malicious misbehavior of validator of the current validator set
+	/// and provides proof of that misbehavor, which varies by engine
+	/// (e.g. double vote).
+	function reportMalicious(address validator, uint256 blockNumber, bytes proof)
+		external;
+
+	/// Get current validator set (last enacted or initial if no changes ever made).
+	function getValidators()
+		external
+		view
+		returns (address[]);
 }
